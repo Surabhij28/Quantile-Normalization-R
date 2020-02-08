8d0ff37fadfca108814d0eb5d4df9e6ef1012c47
Surabhij28 <33096164+Surabhij28@users.noreply.github.com>
2020-02-08 12:48:48 +0100

    Update norm.R

--- norm.R
+++ norm.R
@@ -6,13 +6,16 @@ library(preprocessCore)
 #*************************************************
 # Read raw data
 #************************************************
-Raw<-read.table("RawData.txt",sep = "\t",header = T)
+Raw<-read.table("deino/Rawdat.txt",sep = "\t",header = T)
 #************************************************
 #calculate log2
 #************************************************
 edata = log2(Raw[,-1])
+
+edata[is.na(edata)] <- 0
+
 colramp = colorRampPalette(c(3,"white",2))(142)
-plot(density(edata[,1]),col=colramp[1],lwd=3,ylim=c(0,60),main = "log2 ratio of unormalized data")
+plot(density(edata[,1]),col=colramp[1],lwd=3,ylim=c(0,6),xlim=c(-5,5),main = "log2 ratio of unormalized data")
 for(i in 2:142){lines(density(edata[,i]),lwd=3,col=colramp[i])}
 #*******************************************************
 norm_edata = normalize.quantiles(as.matrix(edata))
@@ -21,4 +24,3 @@ for(i in 2:142){lines(density(norm_edata[,i]),lwd=3,col=colramp[i])}
 #******************************************************
 write.table(norm_edata,file = "NormData.txt",sep = "\t")
 #***********************************************************
-
